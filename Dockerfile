6fe6a847ed138772f1543ffc1255975a9a45001b
Bjørn Erik Lømo <bjorn.lomo@husbanken.no>
2018-05-12 21:07:05 +0200

    Dockerifisering

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,15 @@
-FROM bankmonitor/spring-boot
+FROM openjdk:8-jdk
+
+ENV JAVA_HOME              /usr/lib/jvm/java-8-openjdk-amd64
+ENV JAVA_OPTS              ""
+ENV PATH                   $PATH:$JAVA_HOME/bin
+
+ENV SPRING_PROFILES_ACTIVE test
+
+WORKDIR /app
+
+EXPOSE 8080
+
+COPY target/app.jar /app/app.jar
+
+CMD ["/bin/sh", "-c", "java $JAVA_OPTS -jar /app/app.jar --spring.profiles.active=$SPRING_PROFILES_ACTIVE"]
--- src/main/java/no/boerta/intervju/lanSoknad/LanSoknad.java
+++ src/main/java/no/boerta/intervju/lanSoknad/LanSoknad.java
@@ -2,9 +2,10 @@ package no.boerta.intervju.lanSoknad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LanSoknad {
+public class LanSoknad extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LanSoknad.class, args);
